f8f44111be5ab70f39e66021ca3435ba80c67c35
Adam2604 <adammichala11@gmail.com>
2026-03-05 09:50:53 +0100

    Kliknięcie myszy odbywa się drugą reką

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import cv2
 import mediapipe as mp
 import pyautogui
 import math
+import numpy as np
 
 #Inicjalizacja mediapipe
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
     static_image_mode=False,
-    max_num_hands=1,
+    max_num_hands=2,
     min_detection_confidence=0.7,
     min_tracking_confidence=0.5
 )
@@ -19,11 +20,14 @@ cap = cv2.VideoCapture(0)
 screen_w, screen_h = pyautogui.size()
 
 #Zmienne do wygładzania ruchu
-smoothening = 3
+smoothening = 2
 prev_x, prev_y = 0, 0 
 current_x, current_y = 0, 0
 
-cliked = False
+clicked = False
+
+#Margines aktywnego ekranu
+margin = 120
 
 while cap.isOpened():
     success, frame = cap.read()
@@ -32,46 +36,60 @@ while cap.isOpened():
         break
     #Lustrzane odbicie obrazu
     frame = cv2.flip(frame, 1)
+
+    h,w,c = frame.shape
+    cv2.rectangle(frame, (margin, margin), (w-margin, h-margin), (255,0,255), 2)
     #Konwersja do RGB
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(rgb_frame)
 
     if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
+        has_second_hand = False
+        for i, hand_landmarks in enumerate(results.multi_hand_landmarks):
             #Rysowanie punktów i połączeń na dłoni
             mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-            #Sterowanie kursorem czubkiem palca wskazującego
-            index_finger = hand_landmarks.landmark[8]
-            #Przeliczanie pozycji palca na piksele ekranu
-            cursor_x = int(index_finger.x * screen_w)
-            cursor_y = int(index_finger.y * screen_h)
-            
-            #Wygładzanie ruchu
-            current_x = prev_x + (cursor_x - prev_x) / smoothening
-            current_y = prev_y + (cursor_y - prev_y) / smoothening
-            
-            #Przesunięcie kursora
-            pyautogui.moveTo(current_x, current_y, _pause=False)
+            if i == 0:
+                #Sterowanie kursorem czubkiem palca wskazującego
+                index_finger = hand_landmarks.landmark[8]
+                #Przeliczanie pozycji palca na piksele ekranu
+                x1, y1 = int(index_finger.x * w), int(index_finger.y * h)
 
-            #Aktualizacja pozycji
-            prev_x, prev_y = current_x, current_y
+                cursor_x = np.interp(x1, (margin, w-margin), (0, screen_w))
+                cursor_y = np.interp(y1, (margin, h-margin), (0, screen_h))
+                
+                #Wygładzanie ruchu
+                current_x = prev_x + (cursor_x - prev_x) / smoothening
+                current_y = prev_y + (cursor_y - prev_y) / smoothening
+                
+                #Przesunięcie kursora
+                pyautogui.moveTo(current_x, current_y, _pause=False)
 
-            #Kliknięcie za pomocą kciuka i palca środkowego
-            thumb = hand_landmarks.landmark[4]
-            middle_finger = hand_landmarks.landmark[12]
-            
-            distance = math.hypot(thumb.x - middle_finger.x, thumb.y - middle_finger.y)
-            if distance < 0.05:
-                if not clicked:
-                    pyautogui.click()
-                    clicked = True
-                    #Wizualizacja
-                    h,w,c = frame.shape
-                    cx, cy = int(middle_finger.x * w), int(middle_finger.y * h)
-                    cv2.circle(frame, (cx, cy), 15, (0,255,0), cv2.FILLED)
-            else:
-                clicked = False
+                #Aktualizacja pozycji
+                prev_x, prev_y = current_x, current_y
+
+            elif i == 1:
+                has_second_hand = True
+                #Kliknięcie za pomocą złączenia kciuka i palca wskazującego drugiej dłoni
+                thumb = hand_landmarks.landmark[4]
+                index_finger = hand_landmarks.landmark[8]
+                
+                distance = math.hypot(thumb.x - index_finger.x, thumb.y - index_finger.y)
+                if distance < 0.05:
+                    if not clicked:
+                        pyautogui.click()
+                        clicked = True
+                        #Wizualizacja
+                        h,w,c = frame.shape
+                        cx, cy = int(index_finger.x * w), int(index_finger.y * h)
+                        cv2.circle(frame, (cx, cy), 15, (0,255,0), cv2.FILLED)
+                else:
+                    clicked = False
+                    
+        if not has_second_hand:
+            clicked = False
+    else:
+        clicked = False
     
     cv2.imshow("Hand Gesture Control", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
